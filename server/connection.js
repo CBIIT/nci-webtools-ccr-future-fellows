7c34bdb6c73a3bcee1cb047c55947b9c78868f52
Brian Park <park-brian@users.noreply.github.com>
2018-11-29 00:34:20 -0500

    enable named parameters

--- server/connection.js
+++ server/connection.js
@@ -5,7 +5,8 @@ module.exports = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    namedPlaceholders: true,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
 }).promise();
