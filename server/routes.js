30f009d5bacf5e18ec54db9ffdd89feec222eb3e
Brian Park <park-brian@users.noreply.github.com>
2018-12-03 17:31:48 -0500

    make lodash available in templates

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,15 @@
 const Router = require('koa-router');
 const Body = require('koa-body');
-const { addApplicant } = require('./controllers');
+const lodash = require('lodash');
+const {
+    addApplicant
+} = require('./controllers');
+
 const router = new Router();
 
-// add template variables (ctx.state)
+// add template variables/libraries (ctx.state)
 router.use((ctx, next) => {
+    ctx.state._ = lodash;
     ctx.state.route = ctx._matchedRoute;
     ctx.state.method = ctx.request.method;
     return next();
@@ -13,20 +18,21 @@ router.use((ctx, next) => {
 // use multipart/form-data for the following routes
 router.use(['/apply'], Body({multipart: true}))
 
+// register routes for each page
 router.get('/', ctx => ctx.render('pages/index'));
 
 router.get('/auth', ctx => ctx.render('pages/auth'));
 
 router.get('/apply', ctx => ctx.render('pages/apply', {
     fields: ctx.lookupTables, // use lookup tables for form fields
-    formErrors: null, // form validation errors (null)
-    formValues: {job_category_id: '1'}, // default form values
+    errors: null, // form validation errors (null)
+    values: {job_category_id: '1'}, // default form values
 }));
 
 router.post('/apply', async ctx => ctx.render('pages/apply', {
     fields: ctx.lookupTables, // use lookup tables for form fields
-    formErrors: await addApplicant(ctx), // if no errors, applicant was added successfully
-    formValues: ctx.request.body, // use previously submitted values for form
+    errors: await addApplicant(ctx.request), // if no errors, applicant was added successfully
+    values: ctx.request.body, // use previously submitted values for form
 }));
 
 module.exports = router.routes();
