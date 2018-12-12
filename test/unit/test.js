0914bc0450a49c5b2b14f06f0eba6cbb6f630752
Brian Park <park-brian@users.noreply.github.com>
2018-12-12 17:43:42 -0500

    update unit test imports

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,11 +1,7 @@
-require('dotenv').config();
 const should = require('chai').should();
 const {
     getLookupTables,
-    searchApplicants,
-    addApplicant,
-    updateApplicant
-} = require('../../server/controllers');
+} = require('../../server/controllers/applicants');
 
 describe('Future Fellows', function() {
     it('should correctly fetch lookup tables', async function() {
