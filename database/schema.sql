565fe730f90a3d73641c2fed80747474be871afd
Brian Park <park-brian@users.noreply.github.com>
2018-12-14 15:21:41 -0500

    add ip_address

--- database/schema.sql
+++ database/schema.sql
@@ -1,54 +1,43 @@
-
-
--- lookup table for citizenship types
--- eg: United States, Foreign National, etc
+drop table if exists user_track;
+drop table if exists scientific_focus;
+drop table if exists education_level;
+drop table if exists applicant;
 drop table if exists lu_citizenship;
+drop table if exists lu_education_level;
+drop table if exists lu_job_category;
+drop table if exists lu_scientific_focus;
+drop table if exists lu_state;
+
 create table lu_citizenship
 (
     lu_citizenship_id integer primary key auto_increment,
     name varchar(200) not null
-);
+) comment 'lookup table for citizenship types';
 
--- lookup table for educational degrees
--- eg: DDS, DO, DSc, etc
-drop table if exists lu_education_level;
 create table lu_education_level
 (
     lu_education_level_id integer primary key auto_increment,
     name varchar(20) not null
-);
+) comment 'lookup table for educational degrees';
 
--- lookup table for job categories
--- eg: Postdoctoral Fellowship, Foreign Visiting Fellow, etc
-drop table if exists lu_job_category;
 create table lu_job_category
 (
     lu_job_category_id integer primary key auto_increment,
     name varchar(200) not null
-);
+) comment 'lookup table for job categories';
 
--- lookup table for scientific focus areas
--- eg: Biomedical Engineering, Cancer Biology, etc
-drop table if exists lu_scientific_focus;
 create table lu_scientific_focus
 (
     lu_scientific_focus_id integer primary key auto_increment,
     name varchar(200) not null
-);
+) comment 'lookup table for scientific focus areas';
 
--- look table for U.S. states
--- eg: AK, AL, AR, etc
-drop table if exists lu_state;
 create table lu_state
 (
     lu_state_id integer primary key auto_increment,
-    short_name nchar(2) unique not null
-);
+    name nchar(2) unique not null
+) comment 'lookup table for U.S. states';
 
--- main applicants table
--- contains single-field information about an applicant
--- one-to-many relationship with education_level and scientific_focus
-drop table if exists applicant;
 create table applicant
 (
     applicant_id integer primary key auto_increment,
@@ -74,16 +63,15 @@ create table applicant
     referral_source varchar(2000) not null,
     availability_date date not null,
     resume_filepath varchar(2000) not null,
+    ip_address varchar(40) not null,
     created_date datetime default now(),
     updated_date datetime default now(),
     check (undergraduate_gpa >= 0 and undergraduate_gpa <= 4),
     foreign key (job_category_id) references  lu_job_category(lu_job_category_id),
     foreign key (state_id) references lu_state(lu_state_id),
     foreign key (citizenship_id) references lu_citizenship(lu_citizenship_id)
-);
+) comment 'applicant data, one-to-many with education_level and scientific_focus';
 
--- applicant education levels (many-to-one with applicant)
-drop table if exists education_level;
 create table education_level
 (
     applicant_id integer not null,
@@ -93,10 +81,8 @@ create table education_level
     primary key (applicant_id, lu_education_level_id),
     foreign key (applicant_id) references applicant(applicant_id),
     foreign key (lu_education_level_id) references lu_education_level(lu_education_level_id)
-);
+) comment 'contains each applicant\'s educational degrees';
 
--- applicant scientific focus areas (many-to-one with applicant)
-drop table if exists scientific_focus;
 create table scientific_focus
 (
     applicant_id integer not null,
@@ -106,10 +92,8 @@ create table scientific_focus
     primary key (applicant_id, lu_scientific_focus_id),
     foreign key (applicant_id) references applicant(applicant_id),
     foreign key (lu_scientific_focus_id) references lu_scientific_focus(lu_scientific_focus_id)
-);
+) comment 'contains each applicant\'s scientific focus areas';
 
--- tracks user actions on applicants
-drop table if exists user_track;
 create table user_track
 (
     user_track_id integer primary key auto_increment,
@@ -125,8 +109,7 @@ create table user_track
         'Removed Applicant',
         'Approved Applicant',
         'Searched Applicants'),
-    target text null comment 'email, applicant name, etc.',
+    target varchar(200) null comment 'email, applicant name, etc.',
     created_date datetime default now(),
-    updated_date datetime default now(),
-    foreign key (applicant_id) references applicant(applicant_id),
-);
+    updated_date datetime default now()
+) comment 'tracks user actions';
