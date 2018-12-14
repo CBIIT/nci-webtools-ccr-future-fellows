3c03252915edfbd627a96e2c028cb89afa90cf0e
Brian Park <park-brian@users.noreply.github.com>
2018-12-14 17:35:51 -0500

    update parameters for add_applicant

--- server/controllers/applicants.js
+++ server/controllers/applicants.js
@@ -6,19 +6,71 @@ const isFuture = require('date-fns/is_future')
 const connection = require('../components/connection');
 module.exports = { add, get, search, update };
 
+
 /**
- * Retrieves lookup tables for applicants. These tables are used to
+ * Adds an applicant
+ * @param {Koa Context} context
  */
-async function getLookupTables() {
-    const query = async sql => (await connection.query(sql))[0];
-    return {
-        citizenship: await query('SELECT lu_citizenship_id AS id, name FROM lu_citizenship'),
-        education_level: await query('SELECT lu_education_level_id AS id, name FROM lu_education_level'),
-        job_category: await query('SELECT lu_job_category_id AS id, name FROM lu_job_category'),
-        scientific_focus: await query('SELECT lu_scientific_focus_id AS id, name FROM lu_scientific_focus'),
-        state: await query('SELECT lu_state_id AS id, short_name as name FROM lu_state'),
+async function add(ctx) {
+    const validationErrors = await validateApplicant(ctx);
+    const { body, files } = ctx.request;
+
+    if (!isEmpty(validationErrors))
+        return validationErrors;
+
+    // move resume to uploads folder
+    const filepath = path.join(config.folders.uploads, uuid4() + '.pdf');
+    fs.ensureDirSync(config.folders.uploads);
+    fs.moveSync(files.resume_file.path, filepath);
+
+    // create parameters for stored procedure
+    const join = (c, e) => isArray(e) ? e.filter(c).join() : c(e);
+    const parameters = {
+        ...body,
+        status: ctx.session.authenticated ? body.status : 'PENDING',
+        education_level: join(Number, body.education_level || []),
+        scientific_focus: join(Number, body.scientific_focus || []),
+        resume_filepath: filepath,
+        ip_address: ctx.ip
     };
+
+    // set empty parameters to null
+    for (let key in parameters) {
+        if (parameters[key].length === 0)
+            parameters[key] = null;
+    }
+
+    await connection.execute(`
+        call add_applicant(
+            :job_category_id,
+            :status,
+            :first_name,
+            :middle_initial,
+            :last_name,
+            :email,
+            :address_1,
+            :address_2,
+            :city,
+            :state,
+            :zip,
+            :is_foreign,
+            :home_phone,
+            :work_phone,
+            :fax_phone,
+            :is_foreign,
+            :citizenship_id,
+            :undergraduate_gpa,
+            :research_interests,
+            :postdoc_experience,
+            :referral_source,
+            :availability_date,
+            :resume_filepath,
+            :ip_address,
+            :education_level,
+            :scientific_focus
+        )`, parameters);
 }
+
 
 async function search({body}) {
     const join = (c, e) => isArray(e) ? e.filter(c).join() : c(e);
@@ -49,14 +101,14 @@ function get(query) {
     return {};
 }
 
-async function validate({body, files}) {
-    console.log(body);
+async function validate(ctx) {
+    const { body, files } = ctx.request;
+    const { lookupTables } = ctx;
     const errors = {};
 
     // use ids for lookup values
-    const lookup = mapValues(
-        await getLookupTables(),
-        v => v.map(e => String(e.id))
+    const lookup = mapValues(lookupTables, v =>
+        v.map(e => String(e.id))
     );
 
     // define validator functions (which return true if valid)
@@ -64,29 +116,29 @@ async function validate({body, files}) {
 
     // validators below are nullable (eg: return true if a value is not provided)
     const nullable = f => e => !required(e) || f(e);
-    const pattern = nullable(e => p.test(e));
+    const array = nullable(e => isArray(e));
+    const limit = nullable(len => e => isArrayLike(e) && e.length <= len);
     const range = nullable((min, max) => e => e >= min && e <= max);
-    const inArray = nullable(arr => e => arr.includes(e));
-    const intersectsArray = nullable(arr => e => isEmpty(difference(e, arr)));
-    const limitLength = nullable(len => e => isArrayLike(e) && e.length <= len);
+    const within = nullable(arr => e => arr.includes(e) || isEmpty(difference(e, arr)));
 
     const rules = {
-        job_category_id: [required, inArray(lookup.job_category)],
+        job_category_id: [required, within(lookup.job_category)],
         first_name: [required],
         last_name: [required],
         email: [required],
         address_1: [required],
         city: [required],
-        state_id: [inArray(lookup.state)],
+        state_id: [within(lookup.state)],
+        ip_address: [required],
         home_phone: [required],
-        citizenship_id: [required, inArray(lookup.citizenship)],
+        citizenship_id: [required, within(lookup.citizenship)],
         undergraduate_gpa: [range(0, 4)],
         research_interests: [required],
         postdoc_experience: [required],
         referral_source: [required],
         availability_date: [required, isFuture],
-        education_level: [intersectsArray(lookup.education_level)],
-        scientific_focus: [intersectsArray(lookup.scientific_focus), limitLength(5)],
+        education_level: [array, within(lookup.education_level)],
+        scientific_focus: [array, within(lookup.scientific_focus), limit(5)],
     };
 
     for (let key in rules) {
@@ -103,64 +155,6 @@ async function validate({body, files}) {
     return errors;
 }
 
-async function add({config, request}) {
-    const validationErrors = await validateApplicant(request);
-    const { body, files } = request;
-
-    if (!isEmpty(validationErrors))
-        return validationErrors;
-
-    // move file to uploads folder
-    const filepath = path.join(config.folders.uploads, uuid4() + '.pdf');
-    fs.ensureDirSync(config.folders.uploads);
-    fs.moveSync(files.resume_file.path, filepath);
-
-    // create parameters for stored procedure
-    const join = (c, e) => isArray(e) ? e.filter(c).join() : c(e);
-    const parameters = {
-        ...body,
-        status: 'PENDING',
-        is_foreign: body.state === '',
-        education_level: join(Number, body.education_level || []),
-        scientific_focus: join(Number, body.scientific_focus || []),
-        resume_filepath: filepath
-    };
-
-    for (let key in parameters) {
-        if (parameters[key].length === 0)
-            parameters[key] = null;
-    }
-
-    await connection.execute(`
-        call add_applicant(
-            :job_category_id,
-            :status,
-            :first_name,
-            :middle_initial,
-            :last_name,
-            :email,
-            :address_1,
-            :address_2,
-            :city,
-            :state,
-            :zip,
-            :home_phone,
-            :work_phone,
-            :fax_phone,
-            :is_foreign,
-            :citizenship_id,
-            :undergraduate_gpa,
-            :research_interests,
-            :postdoc_experience,
-            :referral_source,
-            :availability_date,
-            :resume_filepath,
-            :education_level,
-            :scientific_focus
-        )`, parameters);
-
-    return null;
-}
 
 /**
  *
---
+++ server/controllers/lookup_tables.js
@@ -0,0 +1,15 @@
+const connection = require('../components/connection');
+
+/**
+ * Retrieves lookup tables for applicants.
+ */
+async function get() {
+    const query = async sql => (await connection.query(sql))[0];
+    return {
+        citizenship: await query('SELECT lu_citizenship_id AS id, name FROM lu_citizenship'),
+        education_level: await query('SELECT lu_education_level_id AS id, name FROM lu_education_level'),
+        job_category: await query('SELECT lu_job_category_id AS id, name FROM lu_job_category'),
+        scientific_focus: await query('SELECT lu_scientific_focus_id AS id, name FROM lu_scientific_focus'),
+        state: await query('SELECT lu_state_id AS id, name FROM lu_state'),
+    };
+}
