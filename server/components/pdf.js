1d208074113e914e67af4ac5d1f7fbbeb9851844
Brian Park <park-brian@users.noreply.github.com>
2018-12-21 16:54:14 -0500

    update reference to font

--- server/components/pdf.js
+++ server/components/pdf.js
@@ -1,23 +1,11 @@
 const path = require('path');
 const PdfPrinter = require('pdfmake/src/printer');
 const printer = new PdfPrinter({
-    Helvetica: {
-        normal: 'Helvetica',
-        bold: 'Helvetica-Bold',
-        italics: 'Helvetica-Oblique',
-        bolditalics: 'Helvetica-BoldOblique'
-    },
-    Roboto: {
-        normal: path.join(__dirname, 'fonts', 'Roboto-Regular.ttf'),
-        bold: path.join(__dirname, 'fonts', 'Roboto-Medium.ttf'),
-        italics: path.join(__dirname, 'fonts', 'Roboto-Italic.ttf'),
-        bolditalics: path.join(__dirname, 'fonts', 'Roboto-MediumItalic.ttf'),
-    },
     SourceSansPro: {
         normal: path.join(__dirname, 'fonts', 'SourceSansPro-Regular.ttf'),
-        bold: path.join(__dirname, 'fonts', 'SourceSansPro-SemiBold.ttf'),
-        italics: path.join(__dirname, 'fonts', 'SourceSansPro-Italic.ttf'),
-        bolditalics: path.join(__dirname, 'fonts', 'SourceSansPro-MediumItalic.ttf'),
+        bold: path.join(__dirname, 'fonts', 'SourceSansPro-Semibold.ttf'),
+        italics: path.join(__dirname, 'fonts', 'SourceSansPro-It.ttf'),
+        bolditalics: path.join(__dirname, 'fonts', 'SourceSansPro-SemiboldIt.ttf'),
     }
 
 });
