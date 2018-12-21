bbda430538acdd619d99e5a9229775daca01ac41
Brian Park <park-brian@users.noreply.github.com>
2018-12-21 16:31:11 -0500

    update routes for user-track

--- server/routes/user-track.js
+++ server/routes/user-track.js
@@ -1,19 +1,17 @@
 const Router = require('koa-router');
 const users = require('../controllers/users');
-const router = new Router({
-    prefix: '/user-track'
-});
+const router = new Router();
 
 /** User Track Pages */
-router.get('/', async ctx => ctx.render('user-track/users', {
+router.get('/user-track', async ctx => ctx.render('user-track/users', {
     users: await users.list()
 }));
 
-router.get('/:id', async ctx => ctx.render('user-track/user-dates', {
+router.get('/user-track/:id', async ctx => ctx.render('user-track/user-dates', {
     dates: await users.getUserDates(ctx.params.id)
 }));
 
-router.get('/:id/:date', async ctx => ctx.render('user-track/user-actions', {
+router.get('/user-track/:id/:date', async ctx => ctx.render('user-track/user-actions', {
     actions: await users.getUserActions(ctx.params.id, ctx.params.date)
 }));
 
