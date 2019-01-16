6d6694b1f7d34024261847a60ae804740d2bba28
Brian Park <park-brian@users.noreply.github.com>
2019-01-16 14:45:46 -0500

    simplify routes for user-track

--- server/routes/user-track.js
+++ server/routes/user-track.js
@@ -7,12 +7,9 @@ router.get('/user-track', async ctx => ctx.render('user-track/users', {
     users: await users.list()
 }));
 
-router.get('/user-track/:id', async ctx => ctx.render('user-track/user-dates', {
-    dates: await users.getUserDates(ctx.params.id)
-}));
-
-router.get('/user-track/:id/:date', async ctx => ctx.render('user-track/user-actions', {
-    actions: await users.getUserActions(ctx.params.id, ctx.params.date)
+router.get('/user-track/:username', async ctx => ctx.render('user-track/user-actions', {
+    username: ctx.params.username,
+    actions: await users.getActions(ctx.params.username)
 }));
 
 module.exports = router.routes();
